5521d3d674c851d0e3893fcce67f21a9066053e3
Inshaalik <139093211+Inshaalik@users.noreply.github.com>
2025-08-26 13:52:18 -0400

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://raztcxkznbhpbamjlqzr.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhenRjeGt6bmJocGJhbWpscXpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNDc2MTgsImV4cCI6MjA3MTYyMzYxOH0.RWzLhd5nE8XADlBWf0QZDm5aP7iQLUVsvYfCQV0hW1I';
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
