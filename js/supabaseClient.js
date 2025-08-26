f98bc33f00a9e411d56420294f9ba826588c8e59
Inshaalik <139093211+Inshaalik@users.noreply.github.com>
2025-08-26 13:05:02 -0400

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,6 @@
+
 import { createClient } from '@supabase/supabase-js'
 
-// These values come from your Vercel Environment Variables
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseUrl = 'https://raztcxkznbhpbamjlqzr.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
